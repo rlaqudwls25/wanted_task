ca9d9456ec08b7d5b5c7f3cee8a6bc8122c1c390
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-05-08 01:38:29 +0900

    Slider 구현

--- src/routes/Slider/Slider.js
+++ src/routes/Slider/Slider.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Numbers } from '../../Config/config'
+import styles from './Slider.module.scss'
 
 const Slider = () => {
-  return <div>슬라이더</div>
+  const [percent, setPercent] = useState(1)
+
+  const percentChange = (e) => {
+    setPercent(e.target.value)
+  }
+
+  const onClickBtn = (num) => {
+    console.log('num', num)
+    setPercent(num)
+  }
+
+  return (
+    <div className={styles.sliderContainer}>
+      <input className={styles.percentInput} value={percent} readOnly />
+      <div className={styles.percentBox}>%</div>
+      <input className={styles.progressBar} type='range' onChange={percentChange} value={percent} min='1' />
+      <div className={styles.countWrapper}>
+        {Numbers.map((number, idx) => {
+          return (
+            <button type='button' className={styles.countButton} key={idx} onClick={() => onClickBtn(number)}>
+              {number}%
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Slider
--- src/routes/Tab/Tab.js
+++ src/routes/Tab/Tab.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TabMenu } from '../../Config/config'
+import styles from './Tab.module.scss'
 
 const Tab = () => {
-  return <div>텝</div>
+  const [name, setName] = useState('')
+
+  const onClickTab = (item) => {
+    console.log(item)
+    setName(item)
+  }
+  return (
+    <div className={styles.tabContainer}>
+      {TabMenu.map((item, idx) => {
+        return (
+          <button type='button' className={styles.tabBox} key={idx} onClick={() => onClickTab(item)}>
+            {item}
+          </button>
+        )
+      })}
+      <div className={styles.slider} />
+    </div>
+  )
 }
 
 export default Tab
